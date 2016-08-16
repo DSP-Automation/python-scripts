592ae0ed45f14982ee1dfdf03dbe5d16ec4ea81a
dries peeters <dries.peeters@ftsolutions.be>
2016-08-16 12:40:55 +0200

    update

--- python_system_analyzer/analyzer.py
+++ python_system_analyzer/analyzer.py
@@ -1,27 +1,36 @@
 import tkinter as tk
 from tkinter import ttk
 
-import psutils as psu
+import psutil as psu
+
+
 
 class analyzerApp(tk.Tk):
     """docstring for analyzerApp ."""
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.button = ttk.Button(text="Start", command=self.start)
-        self.button.pack()
+        self.cpu_label = tk.Label(self, text="CPU Load:")
+        self.cpu_label.grid(row=0, column=0)
+        self.cpu_label_value = tk.Label(self,text="")
+        self.cpu_label_value.grid(row=0, column=1)
         self.progress = ttk.Progressbar(self, orient="horizontal",length=200, mode="determinate")
-        self.progress.pack()
-        self.cpu_load = 0
-        self.maxbytes = 0
+        self.progress.grid(row=0,column=3)
 
-    def start(self):
+        self.cpu_load = 0
         self.progress['value'] = 0
         self.progress['maximum'] = 100
+        while(1):
+            self.start()
+
+    def start(self):
         self.read_bytes()
+
     def read_bytes(self):
         '''get cpu load'''
         self.cpu_load = psu.cpu_percent(interval=1);
         self.progress['value'] = self.cpu_load
+        self.cpu_label_value['text'] = self.cpu_load
+
 
 app = analyzerApp()
 app.mainloop()
